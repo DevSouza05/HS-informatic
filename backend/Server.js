b5b8cd036837849656bd124684b39ba7c9005cd6
Peterson Moraes de Souza <peterson.moraes@grottone.local>
2025-04-01 10:39:48 -0300

    feat: add router for register users

--- backend/Server.js
+++ backend/Server.js
@@ -1,17 +1,43 @@
-const exprees = require('express');
+const express = require('express');
+const uuid = require('uuid');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
 
 
-const app = exprees();
-const port =  process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.json("Bem-vindo à HS-Ware");
+});
 
-app.use(exprees.json());
+const users = [];
 
 
-app.get('/', (req,res) =>{
-    res.send("Hello World")
-})
+app.post('/users', (req, res) => {
+  const { name, email, password } = req.body;
+
+ 
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: 'Nome, e-mail e senha são obrigatórios!' });
+  }
+
+
+  const user = {
+    id: uuid.v4(),
+    name,
+    email,
+    password
+  };
+
+ 
+  users.push(user);
+
+
+  return res.status(201).json(user);
+});
 
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-  });
+  console.log(`Servidor rodando na porta ${port}`);
+});
